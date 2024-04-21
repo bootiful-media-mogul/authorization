ad214280092dd34620ac8fc3ad95407cd782cca2
Josh Long <josh@joshlong.com>
2024-04-21 13:33:29 +0200

    polish

--- src/main/java/com/joshlong/mogul/authorization/AuthorizationServiceApplication.java
+++ src/main/java/com/joshlong/mogul/authorization/AuthorizationServiceApplication.java
@@ -56,9 +56,9 @@ import java.util.stream.Stream;
  *
  * @author Josh Long
  */
-@EnableConfigurationProperties(AuthorizationApiProperties.class)
+//@EnableConfigurationProperties(AuthorizationApiProperties.class)
 @SpringBootApplication
-@ImportRuntimeHints(AuthorizationServiceApplication.Hints.class)
+//@ImportRuntimeHints(AuthorizationServiceApplication.Hints.class)
 public class AuthorizationServiceApplication {
 
 	static class Hints implements RuntimeHintsRegistrar {
@@ -79,22 +79,12 @@ public class AuthorizationServiceApplication {
 	@Bean
 	@Order(1)
 	SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
-
 		OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-
-		http.getConfigurer(OAuth2AuthorizationServerConfigurer.class).oidc(Customizer.withDefaults()); // Enable
-																										// OpenID
-																										// Connect
-																										// 1.0
-
+		http.getConfigurer(OAuth2AuthorizationServerConfigurer.class).oidc(Customizer.withDefaults());
 		http
-			// Redirect to the login page when not authenticated from the authorization
-			// endpoint
 			.exceptionHandling((exceptions) -> exceptions.defaultAuthenticationEntryPointFor(
 					new LoginUrlAuthenticationEntryPoint("/login"), new MediaTypeRequestMatcher(MediaType.TEXT_HTML)))
-			// Accept access tokens for User Info and/or Client Registration
 			.oauth2ResourceServer((rs) -> rs.jwt(Customizer.withDefaults()));
-
 		return http.build();
 	}
 
@@ -108,6 +98,10 @@ public class AuthorizationServiceApplication {
 
 		return http.build();
 	}
+
+
+
+	/*
 
 	@Bean
 	JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
@@ -202,12 +196,14 @@ public class AuthorizationServiceApplication {
 			});
 		};
 	}
-
+*/
 }
 
-@ConfigurationProperties(prefix = "bootiful.authorization")
+/*
+@ConfigurationProperties(prefix = "mogul.authorization")
 record AuthorizationApiProperties(UserSpecification[] users) {
 }
 
 record UserSpecification(String password, String username, String[] roles) {
 }
+*/
