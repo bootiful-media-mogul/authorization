c2dd52152c83011ee6b2b84e26290b5fcedbd5d8
Josh Long <josh@joshlong.com>
2024-04-22 15:15:42 -0700

    introducing a default user and client

--- src/main/java/com/joshlong/mogul/authorization/AuthorizationServiceApplication.java
+++ src/main/java/com/joshlong/mogul/authorization/AuthorizationServiceApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,12 +14,23 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 
+import java.net.URI;
 import java.util.Set;
 
 /**
@@ -75,89 +87,51 @@ public class AuthorizationServiceApplication {
 			.build();
 	}
 
-	/*
-	 *
-	 * @Bean JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) { return
-	 * new JdbcUserDetailsManager(dataSource); }
-	 *
-	 * @Bean ApplicationRunner usersInitializationRunner(PasswordEncoder passwordEncoder,
-	 * AuthorizationApiProperties properties, UserDetailsManager userDetailsManager) {
-	 * return args -> Stream.of(properties.users()) .peek(e ->
-	 * log.info("registered new user [{}] with password [{}] and roles[{}]", e.username(),
-	 * e.password(), Arrays.toString(e.roles()))) .map(e -> User.builder()
-	 * .passwordEncoder(passwordEncoder::encode) .roles(e.roles()) .username(e.username())
-	 * .password(e.password()) .build()) .filter(u ->
-	 * !userDetailsManager.userExists(u.getUsername()))
-	 * .forEach(userDetailsManager::createUser); }
-	 *
-	 * @Bean PasswordEncoder passwordEncoder() { return
-	 * PasswordEncoderFactories.createDelegatingPasswordEncoder(); }
-	 *
-	 * @Bean JdbcOAuth2AuthorizationConsentService
-	 * jdbcOAuth2AuthorizationConsentService(JdbcOperations jdbcOperations,
-	 * RegisteredClientRepository repository) { return new
-	 * JdbcOAuth2AuthorizationConsentService(jdbcOperations, repository); }
-	 *
-	 * @Bean JdbcOAuth2AuthorizationService jdbcOAuth2AuthorizationService(JdbcOperations
-	 * jdbcOperations, RegisteredClientRepository rcr) { return new
-	 * JdbcOAuth2AuthorizationService(jdbcOperations, rcr); }
-	 *
-	 * @Bean JWKSource<SecurityContext> jwkSource(@Value("${jwk.key.id:bootiful-jwk-id}")
-	 * String id,
-	 *
-	 * @Value("${jwk.key.public}") String publicKeyBase64Encoded,
-	 *
-	 * @Value("${jwk.key.private}") String privateKeyBase64Encoded) throws Exception {
-	 *
-	 * var decoder = Base64.getDecoder(); var publicKey =
-	 * decoder.decode(publicKeyBase64Encoded); var privateKey =
-	 * decoder.decode(privateKeyBase64Encoded);
-	 *
-	 * try (var publicKeyInputStream = new ByteArrayInputStream(publicKey); var
-	 * privateKeyInputStream = new ByteArrayInputStream(privateKey)) { var rsaPrivateKey =
-	 * RsaKeyConverters.pkcs8().convert(privateKeyInputStream); var rsaPublicKey =
-	 * RsaKeyConverters.x509().convert(publicKeyInputStream); var rsa = new
-	 * RSAKey.Builder(rsaPublicKey).privateKey(rsaPrivateKey).keyID(id).build(); var jwk =
-	 * new JWKSet(rsa); return new ImmutableJWKSet<>(jwk); } }
-	 *
-	 * @Bean RegisteredClientRepository registeredClientRepository(JdbcTemplate template)
-	 * { return new JdbcRegisteredClientRepository(template); }
-	 *
-	 * @Bean ApplicationRunner clientsRunner(RegisteredClientRepository repository,
-	 *
-	 * @Value(
-	 * "${TWIS_CLIENT_REDIRECT_URI:http://127.0.0.1:8082/login/oauth2/code/spring}")
-	 * String twisRedirectUri,
-	 *
-	 * @Value("${TWIS_CLIENT_KEY_SECRET}") String twisClientSecret,
-	 *
-	 * @Value("${SOCIALHUB_JOSHLONG_CLIENT_KEY_SECRET}") String socialHubClientSecret) {
-	 * return args -> { var clients = Map.of(
-	 *
-	 * "socialhub-joshlong", RegisteredClient.withId(UUID.randomUUID().toString())
-	 * .clientSecret((socialHubClientSecret))
-	 * .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-	 * .authorizationGrantTypes( grantTypes ->
-	 * grantTypes.add(AuthorizationGrantType.CLIENT_CREDENTIALS)) .scopes(scopes ->
-	 * scopes.addAll(Set.of("user.read", "user.write", OidcScopes.OPENID))), "twis",
-	 * RegisteredClient.withId(UUID.randomUUID().toString())
-	 * .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
-	 * .clientSecret((twisClientSecret))
-	 * .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-	 * .authorizationGrantTypes(grantTypes -> grantTypes
-	 * .addAll(Set.of(AuthorizationGrantType.CLIENT_CREDENTIALS,
-	 * AuthorizationGrantType.AUTHORIZATION_CODE, AuthorizationGrantType.REFRESH_TOKEN)))
-	 * .redirectUri(twisRedirectUri) .scopes(scopes -> scopes.addAll(Set.of("user.read",
-	 * "user.write", OidcScopes.OPENID)))); clients.forEach((clientId, rcb) -> { if
-	 * (repository.findByClientId(clientId) == null)
-	 * repository.save(rcb.clientId(clientId).build()); }); }; }
-	 */
+	@Bean
+	InMemoryUserDetailsManager users(PasswordEncoder passwordEncoder,
+			@Value("${AUTHORIZATION_SERVICE_USERS_JLONG_USERNAME:jlong}") String username,
+			@Value("${AUTHORIZATION_SERVICE_USERS_JLONG_PASSWORD:pw}") String password) {
+		// todo
+		log.info("got the following users: " + username);
+		var user = User.withUsername(username)//
+			.password(passwordEncoder.encode(password))//
+			.roles("user", "admin")//
+			.build();
+		return new InMemoryUserDetailsManager(user);
+	}
+
+	@Bean
+	RegisteredClientRepository registeredClients(PasswordEncoder passwordEncoder,
+			@Value("${AUTHORIZATION_SERVICE_CLIENTS_MOGUL_CLIENT_ID:mogul}") String clientId,
+			@Value("${AUTHORIZATION_SERVICE_CLIENTS_MOGUL_CLIENT_SECRET:mogul}") String clientSecret,
+			@Value("${MOGUL_GATEWAY_HOST:http://127.0.0.1:1010}/login/oauth2/code/spring") URI redirectUri) {
+		// todo remove this
+		log.info("DEBUG: " + clientId + ":" + redirectUri);
+		var rc = RegisteredClient.withId(clientId)
+			.clientId(clientId)
+			.authorizationGrantTypes(c -> c
+				.addAll(Set.of(AuthorizationGrantType.AUTHORIZATION_CODE, AuthorizationGrantType.REFRESH_TOKEN)))
+			.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+			.clientSecret(passwordEncoder.encode(clientSecret))
+			.redirectUri(redirectUri.toString())
+			.scopes(c -> c.addAll(Set.of("user.read", "user.write", "openid")))
+			.clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+			.build();
+		return new InMemoryRegisteredClientRepository(rc);
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
 
 }
 
 /*
+ *
  * @ConfigurationProperties(prefix = "mogul.authorization") record
  * AuthorizationApiProperties(UserSpecification[] users) { }
  *
  * record UserSpecification(String password, String username, String[] roles) { }
+ *
  */
